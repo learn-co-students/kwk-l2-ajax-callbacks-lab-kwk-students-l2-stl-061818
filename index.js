b960a26334f9981e500f7d201d54b5e90c90cd0b
abbylee1 <ablee@micds.org>
2018-06-26 15:29:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 $(document).ready(function (){
 });
+
+function searchRepositories() {
+  const searchTerms = $("#searchTerms").val();
+  $.get(`https://api.github.com/search/repositories?q
+    =${searchTerms}` , data => {
+      S(`#results`).html();
+    })
+  
+}
+
+var renderSearchResults = (data) => data.items.map(results =>
+  renderSearchResults(result))
+  
+var renderSearchResults = (result) => 
